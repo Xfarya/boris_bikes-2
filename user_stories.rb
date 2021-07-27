1c281a88571456145f48435b10fd35c2a5dfb252
Farya <farya.hsn@gmail.com>
2021-07-27 16:18:20 +0200

    user stories diagram added

---
+++ user_stories.rb
@@ -0,0 +1,10 @@
+# Write down all the nouns in the User Stories
+# Write down all the verbs in the User Stories
+# Draw a table like the one above
+# Organise the nouns and verbs into Objects and Messages within the table
+# Draw a diagram that shows how your Objects will use Messages to communicate with one another
+
+Object          || Messages
+person          || use_a_bike
+docking_station || release_a_bike
+bike            || is_bike_working

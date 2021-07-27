16d229330c32401554bb4d97d6a3f476d7177ada
Farya <farya.hsn@gmail.com>
2021-07-27 17:00:22 +0200

    step4

--- user_stories.rb
+++ user_stories.rb
@@ -5,6 +5,27 @@
 # Draw a diagram that shows how your Objects will use Messages to communicate with one another
 
 Object          || Messages
-person          || use_a_bike
+person          || 
 docking_station || release_a_bike
 bike            || is_bike_working
+
+
+# Write down the type of error
+# 1. NameError (uninitialized constant DockingStation)
+
+# Write down the file path where the error happened
+
+# 3: from /Users/faryahussain/.rvm/rubies/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/irb-1.3.0/exe/irb:11:in `<top (required)>'
+
+# Write down the line number of the error
+# 11
+
+# Use the Ruby Documentation to find out what the error means
+# You'll see this error when the code refers to a class or module that it can't find, often because the code doesn't include require, which instructs the Ruby file to load the class.
+# In Ruby, variables/methods begin with lowercase letters, while classes begin with uppercase letters. If the code doesn't reflect this distinction, you'll receive the Uninitialized Constant exception.
+
+
+
+# Suggest one way of solving the error.
+# - issue is because we don't have class - so either create a class, or link to a class
+

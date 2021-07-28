aa55a0e494dfe71aa2e0d08bc86e75f2c164eaa8
edpackard <edpackard81@gmail.com>
2021-07-28 15:56:48 +0100

    Chapter 10 wrote and passed test a) to get a bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,13 +1,14 @@
 require 'dockingstation'
-# describe DockingStation do
-#     # # Hard coded tests
-#     # it 'returns "dock here" when passed King Cross station' do
-#     #     expect(dockingstation("king cross station")).to eq 'dock here'
-#     # end
-# end
 
 describe DockingStation do
 
     it { is_expected.to respond_to(:release_bike) }
+
+    describe DockingStation.new.release_bike do
+      it { is_expected.to be_a(Bike) }
+    end
+
+# if release bike is called on an instance of DS == Bike.new ?
+# if working ? = true
 
 end

6523fc95e22dc328d700cd119de3638dd819b1fc
edpackard <edpackard81@gmail.com>
2021-07-28 18:19:25 +0100

    Added chapter 11 tests and code

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,13 +1,17 @@
 class DockingStation
 
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
   def dock_bike(bike)
-    bike
+    @bike = bike
   end
+  
+  # def bike
+  #   @bike
+  # end
 
 end
-
-
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -10,9 +10,17 @@ describe DockingStation do
 
     it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
-    it 'returns the value of bike' do
+    it { is_expected.to respond_to(:bike) }
+
+    it 'docks something' do
       bike = Bike.new
-      expect(DockingStation.new.dock_bike(bike)).to eq(bike)
+      expect(subject.dock_bike(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bike).to eq bike
     end
 
 end

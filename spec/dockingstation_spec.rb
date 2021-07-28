5e3daae88d10a99805fadf8f8732d641f3e48bff
Farya <farya.hsn@gmail.com>
2021-07-28 17:19:51 +0200

    Chapter 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,8 +4,8 @@ describe DockingStation do
 
     it { is_expected.to respond_to(:release_bike) }
 
-    describe DockingStation.new.release_bike do
-      it { is_expected.to be_a(Bike) }
+    it 'is working' do
+      expect(DockingStation.new.release_bike.working?).to eq true
     end
 
 # if release bike is called on an instance of DS == Bike.new ?
